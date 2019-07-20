2292ba8c5accfa8dc4a162993398f9ff9fba5800
smritibhardwaj01 <41472997+smritibhardwaj01@users.noreply.github.com>
2019-07-20 09:29:41 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,37 +1,55 @@
-(function(){
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
+function getUiConfig() {
+  return {
+    'callbacks': {
+      'signInSuccess': function(user, credential, redirectUrl) {
+        handleSignedInUser(user);
+        return false;
       }
     },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: 'index.html',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+    'signInFlow': 'popup',
+    'signInOptions': [
+	
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+	{
+        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        recaptchaParameters: {
+          type: 'image', 
+          size: 'invisible',
+          badge: 'bottomleft' 
+        },
+	      defaultCountry: 'IN', 
+	      defaultNationalNumber: '1234567890',
+	      loginHint: '+11234567890'
+	}
+        ],
+    'tosUrl': 'https://www.google.com'
   };
+}
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-})()
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'block';
+  document.getElementById('user-signed-out').style.display = 'none';
+  document.getElementById('phone').textContent = user.phoneNumber;
+};
+
+var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  ui.start('#firebaseui-container', getUiConfig());
+};
+
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+var initApp = function() {
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+};
+
+window.addEventListener('load', initApp);
